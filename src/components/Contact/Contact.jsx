12de65b90efc4110a3fda86e69d25d1101c32f30
Anjali <anjalirajput20082003@gmail.com>
2024-07-24 17:49:03 +0530

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <a href="mailto:anjalirajput20082003@gmail.com">
+            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+            anjalirajput20082003@gmail.com
+          </a>
+        </li>
+        <li className={styles.link}>
+          <a href="https://www.linkedin.com/in/anjali-955b2623a/">
+            <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon" />
+            https://www.linkedin.com/in/anjali-955b2623a/
+          </a>
+        </li>
+        <li className={styles.link}>
+          <a href="https://github.com/anjali20082003">
+            <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+            https://github.com/anjali20082003
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,20 @@
+// Resume.jsx
+import React from 'react';
+import styles from './Resume.module.css';
+
+const Resume = () => {
+  return (
+    <div className={styles.resumeContainer} id="resume">
+      <h1 className={styles.heading}>My Resume</h1>
+      <p className={styles.description}>
+        Click the link below to view or download my resume.
+      </p>
+      <a href="assets\resume\Resume.pdf" target="_blank" rel="noopener noreferrer" className={styles.resumeLink}>
+        
+        View Resume
+      </a>
+    </div>
+  );
+};
+
+export default Resume;
