d43476ace6dda474ad7f76d662310edb71f94e21
Anjali <anjalirajput20082003@gmail.com>
2024-10-01 22:25:26 +0530

    Deploying latest changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 
-// Using process.env.PUBLIC_URL for flexible basename
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
